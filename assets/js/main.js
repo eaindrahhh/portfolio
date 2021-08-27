038588749faa9a5216de3df97b92dbf98c6934f5
eaindrahhh <eaindra54321@gmail.com>
2021-08-27 16:45:20 +0630

    update profile image and qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,14 +29,32 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 function toggleSkills(){
     let itemClass = this.parentNode.className
     for(i=0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
+        skillsContent[i].className = 'skills__content skills__open'
     }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
+    if(itemClass === 'skills__content skills__open'){
+        this.parentNode.className = 'skills__content skills__close'
     }
 }
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
+})
+// QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
 })
 // SERVICE MODAL
 const modalViews = document.querySelectorAll('.services__modal'),
